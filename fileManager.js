8a4c67f45fac587002ba8ab693d9886da764d097
EVISION\Otabek.Mirzakhmedov <otabek.mirzakhmedov@wolterskluwer.com>
2024-02-04 16:48:20 +0500

    feat: hash and compression added

--- fileManager.js
+++ fileManager.js
@@ -1,120 +1,136 @@
 const {
-  printCurrentDirectory,
-  displayWelcomeMessage,
-  displayGoodbyeMessage,
-  handleInvalidInput,
+    printCurrentDirectory,
+    displayWelcomeMessage,
+    displayGoodbyeMessage,
+    handleInvalidInput,
 } = require("./utils");
 const { listFilesAndFolders } = require("./directoryListing");
 const {
-  changeWorkingDirectory,
-  navigateToParentDirectory,
+    changeWorkingDirectory,
+    navigateToParentDirectory,
 } = require("./navigation");
 const {
-  readFile,
-  createFile,
-  renameFile,
-  copyFile,
-  moveFile,
-  deleteFile,
+    readFile,
+    createFile,
+    renameFile,
+    copyFile,
+    moveFile,
+    deleteFile,
 } = require("./fileOperations");
 const {
-  getEOL,
-  getCPUsInfo,
-  getHomeDirectory,
-  getSystemUserName,
-  getCPUArchitecture,
+    calculateHash,
+} = require("./hashOperations");
+const {
+    compressFile,
+    decompressFile,
+} = require("./compressionOperations");
+const {
+    getEOL,
+    getCPUsInfo,
+    getHomeDirectory,
+    getSystemUserName,
+    getCPUArchitecture,
 } = require("./osOperations");
 
 function startFileManager(username) {
-  displayWelcomeMessage(username);
+    displayWelcomeMessage(username);
 
-  printCurrentDirectory();
+    printCurrentDirectory();
 
-  process.stdin.on("data", (input) => {
-    const userInput = input.toString().trim();
-    const [command, ...args] = userInput.split(" ");
+    process.stdin.on("data", async (input) => {
+        const userInput = input.toString().trim();
+        const [command, ...args] = userInput.split(" ");
 
-    switch (command) {
-      case ".exit":
+        switch (command) {
+            case ".exit":
+                displayGoodbyeMessage(username);
+                process.exit(0);
+                break;
+            case "up":
+                navigateToParentDirectory();
+                printCurrentDirectory();
+                break;
+            case "cd":
+                changeWorkingDirectory(args[0]);
+                printCurrentDirectory();
+                break;
+            case "ls":
+                listFilesAndFolders();
+                printCurrentDirectory();
+                break;
+            case "cat":
+                readFile(args[0]);
+                break;
+            case "add":
+                createFile(args[0]);
+                break;
+            case "rn":
+                renameFile(args[0], args[1]);
+                break;
+            case "cp":
+                await copyFile(args[0], args[1]);
+                printCurrentDirectory();
+                break;
+            case "mv":
+                await moveFile(args[0], args[1]);
+                printCurrentDirectory();
+                break;
+            case "rm":
+                deleteFile(args[0]);
+                break;
+            case "cwd":
+                printCurrentDirectory();
+                break;
+            case "os":
+                handleOSCommand(args);
+                break;
+            case "hash":
+                await calculateHash(args[0]);
+                printCurrentDirectory();
+                break;
+            case "compress":
+                await compressFile(args[0], args[1]);
+                printCurrentDirectory();
+                break;
+            case "decompress":
+                await decompressFile(args[0], args[1]);
+                printCurrentDirectory();
+                break;
+            default:
+                handleInvalidInput();
+                printCurrentDirectory();
+        }
+    });
+
+    process.on("SIGINT", () => {
+        console.log();
         displayGoodbyeMessage(username);
         process.exit(0);
-        break;
-      case "up":
-        navigateToParentDirectory();
-        printCurrentDirectory();
-        break;
-      case "cd":
-        changeWorkingDirectory(args[0]);
-        printCurrentDirectory();
-        break;
-      case "ls":
-        listFilesAndFolders();
-        printCurrentDirectory();
-        break;
-      case "cat":
-        readFile(args[0]);
-        break;
-      case "add":
-        createFile(args[0]);
-        break;
-      case "rn":
-        renameFile(args[0], args[1]);
-        break;
-      case "cp":
-        copyFile(args[0], args[1]);
-        break;
-      case "mv":
-        moveFile(args[0], args[1]);
-        break;
-      case "rm":
-        deleteFile(args[0]);
-        break;
-      case "cwd":
-        printCurrentDirectory();
-        break;
-      case "os":
-        handleOSCommand(args);
-        break;
-      default:
-        handleInvalidInput();
-    }
-  });
-
-  process.on("SIGINT", () => {
-    console.log();
-    displayGoodbyeMessage(username);
-    process.exit(0);
-  });
+    });
 }
 
 function handleOSCommand(args) {
-  const [operation, ...rest] = args;
+    const [operation, ...rest] = args;
 
-  switch (operation) {
-    case "--EOL":
-      getEOL();
-      printCurrentDirectory();
-      break;
-    case "--cpus":
-      getCPUsInfo();
-      printCurrentDirectory();
-      break;
-    case "--homedir":
-      getHomeDirectory();
-      printCurrentDirectory();
-      break;
-    case "--username":
-      getSystemUserName();
-      printCurrentDirectory();
-      break;
-    case "--architecture":
-      getCPUArchitecture();
-      printCurrentDirectory();
-      break;
-    default:
-      handleInvalidInput();
-      printCurrentDirectory();
-  }
+    switch (operation) {
+        case "--EOL":
+            getEOL();
+            break;
+        case "--cpus":
+            getCPUsInfo();
+            break;
+        case "--homedir":
+            getHomeDirectory();
+            break;
+        case "--username":
+            getSystemUserName();
+            break;
+        case "--architecture":
+            getCPUArchitecture();
+            break;
+        default:
+            handleInvalidInput();
+    }
 }
 
 module.exports = { startFileManager };
